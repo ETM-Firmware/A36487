fd50f523af937aa297241d669da8ec0c68171489
dparkermit <dparker@etm-inc.com>
2019-12-09 14:34:36 -0800

    Adding -008 for 2.5 MeV

--- A36487.X/FIRMWARE_VERSION.h
+++ A36487.X/FIRMWARE_VERSION.h
@@ -1,9 +1,24 @@
 #ifndef __FIRMWARE_VERISON_H
 #define __FIRMWARE_VERISON_H
 
+#define __BUILD_DASH_008
+
+#ifdef __BUILD_DASH_008
+
+#define FIRMWARE_AGILE_REV     1
+#define FIRMWARE_BRANCH        0
+#define FIRMWARE_BRANCH_REV    0
+#define FIRMWARE_DASH_NUMBER   8
+
+#else
+
 #define FIRMWARE_AGILE_REV     5
 #define FIRMWARE_BRANCH        0
 #define FIRMWARE_BRANCH_REV    1
+#define FIRMWARE_DASH_NUMBER   251
+
+#endif
+
 
 
 #endif
