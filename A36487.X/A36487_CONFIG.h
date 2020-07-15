b68c7ece63f348fe768860d15c84216274209597
dparkermit <dparker@etm-inc.com>
2020-07-14 17:08:42 -0700

    Magnet TX Updates
    Update?

--- A36487.X/A36487_CONFIG.h
+++ A36487.X/A36487_CONFIG.h
@@ -9,7 +9,7 @@
 
 
 // DPARKER import the compile options here so these are only valid on external trigger mode
-#define MAX_TRIGGER_HIGH_TIME_TMR1_UNITS        100  // Each TMR1 unit is 6.4us so this is 640uS
+#define MAX_TRIGGER_HIGH_TIME_TMR1_UNITS        62500 // 100  // Each TMR1 unit is 6.4us so this is 640uS
 #define TRIGGER_STAYED_HIGH_FAULT_LEVEL         5
 #define TRIGGER_NOT_VALID_FAULT_LEVEL           5
 #define TRIGGER_PERIOD_TOO_SHORT_FAULT_LEVEL    5
